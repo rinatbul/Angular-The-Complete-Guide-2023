83e24faa921ffaaf617ab6e1f0b844018c3dfe76
Rinat <rinatbuldybayev@gmail.com>
2024-02-27 08:17:33 +0600

    22. Using Template in component.ts file

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+    <app-server/>
+    <app-server/>`,
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
