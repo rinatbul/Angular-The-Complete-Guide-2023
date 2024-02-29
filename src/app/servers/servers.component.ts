e27eaac01b05245f4c30bf8e1cc6a2253646f084
Rinat <rinatbuldybayev@gmail.com>
2024-02-29 08:57:20 +0600

    28. Property Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,11 +3,17 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-servers',
   // selector: '.app-servers',
-  template: `
-    <app-server/>
-    <app-server/>`,
+  // template: `
+  //   <app-server/>
+  //   <app-server/>`,
+  templateUrl:'./servers.component.html',
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
-
+    allowNewServer = false;
+    constructor() {
+      setTimeout(()=>{
+        this.allowNewServer = true;
+      },2000)
+    }
 }
